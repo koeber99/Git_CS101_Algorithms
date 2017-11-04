d1fb99e9d65ec7d7509dcb45ed11fd0e8a6a84b0
koeber99@gmail.com <koeber99@gmail.com>
2017-11-03 20:27:31 -0400

    Update Is_ PowerOf
    Update Is_ PowerOf

--- src/Basic_Math_Functions/IsPowerOf.java
+++ src/Basic_Math_Functions/IsPowerOf.java
@@ -3,6 +3,12 @@ package Basic_Math_Functions;
 public class IsPowerOf {
 	
 	
+	//Run Time O(1) 
+	//isPowerOfTwo or Zero
+	public static boolean isPowerOfTwo (int num){
+		return ( ((num& (num-1))==0)  ) ;
+	}
+	
 	//Run Time O(1)
 	public static boolean isPowerOfFour (int num){
 		return ( (num >0)&&   ((num& (num-1))==0) && (num-1)%3==0   ) ;
